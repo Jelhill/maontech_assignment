6460bc45a2909ced68beb44cb382f08fd6422b4d
Jelhill <talk2jelili2000@gmail.com>
2021-02-13 23:03:15 +0100

    Added a home route for heoku to load

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,14 +1,9 @@
 const router = require("express").Router();
-const User = require("../Schema/userSchema")
-const jwt = require("jsonwebtoken")
-const keys = require('../Config/keys')
-const bcrypt = require('bcrypt')
-const validator = require('validator')
 const authUser = require("../middleware/authUser")
 const userController = require("../controllers/userController")
 const questionController = require("../controllers/questionController")
 
-
+router.get("/", (req, res) => res.status(200).json({success: true, message:"App is working fine"}))
 router.post("/register", userController.register);
 router.post("/login", userController.login)
 router.post("/addQuestion", authUser, userController.addQuestion);
